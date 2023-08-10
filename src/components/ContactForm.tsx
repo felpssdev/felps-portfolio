f498a9cd4d72d36ad7041aec9e2d14b7275f0047
Felipe <felipeicloud11@gmail.com>
2023-08-10 16:53:39 -0300

    fix: set default notify state to false

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { useForm, Controller } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
