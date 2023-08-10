4eb77eef8dc5a8debf91e3cf37418a90c1010953
Felipe <felipeicloud11@gmail.com>
2023-08-10 15:08:31 -0300

    style: improve header and footer responsive design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,17 +4,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-black py-6 px-24 flex flex-col gap-4 items-center justify-center z-10'>
+    <div className='bg-black py-3 px-24 text-sm 2xl:py-6 2xl:px-24 flex flex-col gap-2 2xl:gap-4 items-center justify-center z-10'>
       <div className='flex gap-4'>
-        <Link href="https://github.com/felpssdev" target='blank' className='text-black bg-white w-10 h-10 rounded-full flex items-center justify-center transition-all duration-100 cursor-pointer hover:scale-125 hover:text-purple-800'>
+        <Link href="https://github.com/felpssdev" target='blank' className='text-black bg-white w-8 h-8 2xl:w-10 2xl:h-10 rounded-full flex items-center justify-center transition-all duration-100 cursor-pointer hover:scale-125 hover:text-purple-800'>
           <Github />
         </Link>
-        <Link href="https://www.linkedin.com/in/felipesantos-dev/" target='blank' className='text-black bg-white w-10 h-10 rounded-full flex items-center justify-center transition-all duration-100 cursor-pointer hover:scale-125 hover:text-purple-800'>
+        <Link href="https://www.linkedin.com/in/felipesantos-dev/" target='blank' className='text-black bg-white w-8 h-8 2xl:w-10 2xl:h-10 rounded-full flex items-center justify-center transition-all duration-100 cursor-pointer hover:scale-125 hover:text-purple-800'>
           <Linkedin />
         </Link>
       </div>
       <div className='text-white'>
-        <span className='text-sm font-bold'>Felipe S. - © 2023</span>
+        <span className='text-xs 2xl:text-sm font-bold'>Felipe S. - © 2023</span>
       </div>
     </div>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import Navbar from './Navbar'
 
 const Header = () => {
   return (
-    <div className='px-10 py-4 bg-black text-white flex items-center justify-between z-20 shadow-lg shadow-black'>
-      <div className='flex gap-2 font-bold text-3xl'>
+    <div className='px-10 py-4 md:py-2 2xl:py-4 bg-black text-white flex items-center justify-between z-20'>
+      <div className='flex gap-2 font-bold text-xl 2xl:text-3xl'>
         <span>Felipe</span>
         <span className='text-purple-800'>Santos</span>
       </div>
